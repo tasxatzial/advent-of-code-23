2115ff96d1c506337382a50e345038d6af4c9eb6
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-12-01 11:01:52 +0200

    day1 p2

--- day01/src/day01/core.clj
+++ day01/src/day01/core.clj
@@ -17,15 +17,32 @@
 ; --------------------------
 ; problem 1
 
-(defn get-first-num
+(defn p1_get-first-num
   [calibration-line]
   (Integer/parseInt (re-find #"\d" calibration-line)))
 
-(defn extract-calibration-value
+(defn p1_extract-calibration-value
   [calibration-line]
-  (let [first-num (get-first-num calibration-line)
-        last-num (get-first-num (clojure.string/reverse calibration-line))]
+  (let [first-num (p1_get-first-num calibration-line)
+        last-num (p1_get-first-num (clojure.string/reverse calibration-line))]
     (+ (* 10 first-num) last-num)))
+
+; --------------------------
+; problem 2
+
+(def digit-str->vals
+  (zipmap ["1" "2" "3" "4" "5" "6" "7" "8" "9" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine"]
+          (take 18 (cycle (range 1 10)))))
+
+(def digits-pattern
+  (re-pattern "(?=(one|two|three|four|five|six|seven|eight|nine|\\d))."))
+
+(defn p2_extract-calibration-value
+  [calibration-line]
+  (let [digits (map digit-str->vals (map second (re-seq digits-pattern calibration-line)))]
+    (if (= 1 (count digits))
+      (+ (* 10 (first digits)) (first digits))
+      (+ (* 10 (first digits)) (last digits)))))
 
 ; --------------------------
 ; results
@@ -33,9 +50,16 @@
 (defn day01-1
   []
   (->> (memoized-input-file->calibration-lines)
-       (map extract-calibration-value)
+       (map p1_extract-calibration-value)
+       (reduce +)))
+
+(defn day01-2
+  []
+  (->> (memoized-input-file->calibration-lines)
+       (map p2_extract-calibration-value)
        (reduce +)))
 
 (defn -main
   []
-  (println (day01-1)))
+  (println (day01-1))
+  (println (day01-2)))
