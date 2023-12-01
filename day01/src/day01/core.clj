78230bf3fdea47550e047bb0d1c29ca4bf9ab274
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-12-01 07:21:35 +0200

    day1: parse file

--- day01/src/day01/core.clj
+++ day01/src/day01/core.clj
@@ -1,6 +1,19 @@
 (ns day01.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn input-file->calibration-lines
+  []
+  (-> input-file
+      slurp
+      clojure.string/split-lines))
+
+(def memoized-input-file->calibration-lines (memoize input-file->calibration-lines))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
